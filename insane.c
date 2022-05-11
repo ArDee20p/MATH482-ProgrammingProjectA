eab40c44f2d36dfe3a5596a4699f15bb7818bb7a
Ben Burrill <bburrill98@gmail.com>
2022-05-10 19:30:33 -0700

    Read puzzles from file in insane.c

--- insane.c
+++ insane.c
@@ -196,108 +196,20 @@ int findSolFlip(int n, int colors, int puz[][3]) {
 }
 
 
+int main(int argc, char *argv[]) {
 
-int puzzleSlices[6][14][3] = {
-    //Puzzle One
-    12,     9,      7,
-    4,      2,      13,
-    11,     8,      6,
-    3,      1,      12,
-    10,     7,      5,
-    2,      14,     11,
-    9,      6,      4,
-    1,      13,     10,
-    7,      5,      2,
-    14,     11,     9,
-    6,      4,      1,
-    13,     10,     8,
-    5,      3,      14,
-    12,     8,      3,
-    //Puzzle Two
-    9,      4,      13,
-    8,      3,      12,
-    7,      2,      11,
-    6,      1,      10,
-    5,      14,     9,
-    4,      13,     8,
-    2,      11,     6,
-    1,      10,     5,
-    14,     9,      4,
-    13,     8,      3,
-    12,     7,      2,
-    11,     6,      1,
-    3,      12,     7,
-    10,     5,      14,
-    //Puzzle Three
-    11,     7,      4,
-    14,     10,     7,
-    3,      13,     10,
-    6,      2,      13,
-    9,      5,      2,
-    12,     8,      5,
-    1,      11,     8,
-    4,      14,     11,
-    7,      3,      14,
-    10,     6,      3,
-    13,     9,      6,
-    2,      12,     9,
-    5,      12,     8, 
-    1,      4,      1,
-    //Puzzle Four
-    3,      5,      8,
-    10,     13,     1,
-    3,      6,      8,
-    11,     13,     2,
-    4,      6,      9,
-    11,     14,     2,
-    5,      7,      9,
-    12,     14,     3,
-    5,      8,      10,
-    12,     1,      6,
-    11,     13,     1,
-    4,      9,      14,
-    2,      4,      7,
-    12,     7,      10,
-    //Puzzle Five
-    4,      8,      12,
-    2,      6,      10, 
-    14,     4,      8, 
-    11,     1,      5, 
-    9,      13,     3, 
-    7,      11,     1, 
-    4,      8,      12, 
-    2,      6,      10, 
-    14,     11,     1,
-    5,      9,      13, 
-    3,      7,      12, 
-    2,      6,      10, 
-    14,     5,      9, 
-    13,     3,      7,
-    //Puzzle Six
-    11,     8,      5,
-    2,      13,     10,
-    7,      4,      1,
-    12,     9,      6,
-    3,      14,     11,
-    8,      4,      1,
-    12,     9,      6,
-    3,      14,     11,
-    8,      5,      2,
-    13,     10,     7,
-    4,      1,      12,
-    5,      2,      13,
-    10,     7,      9,
-    6,      3,      14
-};
+    if (argc < 2) printf("Put a \n");
+    int (*puz)[3];
+    int size = readPuzFile(argv[1], &puz);
 
-
-int main() {
-    for (int i = 0; i < 6; i++) {
-        if (findSolPar(14, 14, puzzleSlices[i], 2)) {
-            printf("Found solution for puzzle %d\n", i+1);
-            writePuz(stdout, 14, puzzleSlices[i]);
-        } else {
-            printf("No solution for puzzle %d\n", i+1);
-        }
+    if (findSolPar(size, size, puz, 2)) {
+        printf("Found solution for %s\n", argv[1]);
+        writePuz(stdout, size, puz);
+    } else {
+        printf("No solution for puzzle %s\n", argv[1]);
     }
+
+    free(puz);
+
+    return 0;
 }
